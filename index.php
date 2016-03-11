4c43c3606895c46d81acbda81b578e7177fffb9a
Victor Forato <victorfdt@yahoo.com.br>
2016-03-11 14:37:49 -0300

    Adding the structure of the project

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">		
+		<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+
+		<!-- JavaScript -->
+		<script src="js/angular.js"></script>
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/jquery-ui.js"></script>
+		
+		<title>MindKeeper</title>
+	</head>
+	
+	<body>
+		<p>This is my web page</p>
+	</body>
+
+</html>
+
+
+
